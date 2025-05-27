e98aef4d91adc9886e26e02bccee31e682a24007
that70schris <chris@bailey.mx>
2025-05-27 02:35:32 +0000

    updates

--- .hammerspoon/Spoons/browser.spoon/init.lua
+++
@@ -1,8 +0,0 @@
-hs.urlevent.httpCallback = function(scheme, host, params, url)
-  local app = 'com.google.Chrome'
-  if hs.eventtap.checkKeyboardModifiers().shift then
-    app = 'com.apple.Safari'
-  end
-
-  hs.urlevent.openURLWithBundle(url, app)
-end
