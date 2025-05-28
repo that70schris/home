b31a33bda1c938dd7d6483fd810317c6a4be1a5e
that70schris <chris@bailey.mx>
2025-05-28 15:53:26 +0000

    updates

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,5 @@
 hs.pathwatcher.new('../.hammerspoon', hs.reload):start()
 hs.allowAppleScript(true)
-hs.execute(
-  'open raycast://extensions/maxnyby/raycast-notification/index?arguments=%7B%22message%22%3A%22%22%2C%22title%22%3A%22reloading%22%2C%22type%22%3A%22%22%7D')
 
 hs.loadSpoon('escape')
 hs.loadSpoon('enter')
@@ -15,3 +13,11 @@ if spoon.Hammerflow.auto_reload then
   hs.loadSpoon("ReloadConfiguration")
   spoon.ReloadConfiguration:start()
 end
+
+hs.notify.show(
+  'Configuration Reloaded',
+  '',
+  ''
+)
+-- hs.execute(
+--   'open raycast://extensions/maxnyby/raycast-notification/index?arguments=%7B%22title%22%3A%22reloading%22%7D')
