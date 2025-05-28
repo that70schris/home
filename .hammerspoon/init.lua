c1ba3ff7fe48e9cf2312fbd5cd95c67ecbe39179
that70schris <chris@bailey.mx>
2025-05-28 14:58:01 +0000

    updates

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,21 @@
-hs.pathwatcher.new('../.hammerspoon', hs.reload):start()
+hs.pathwatcher.new('../.hammerspoon', function()
+  hs.execute(
+    'open raycast://extensions/bjrmatos/hammerspoon/reload-configuration-file')
+end):start()
 hs.allowAppleScript(true)
+hs.alert('loading')
 
 hs.loadSpoon('escape')
 hs.loadSpoon('enter')
 hs.loadSpoon('clear')
-hs.loadSpoon('Hammerflow')
-spoon.Hammerflow.loadFirstValidTomlFile({
-  "flow.toml",
-})
+-- hs.loadSpoon('Hammerflow')
+-- spoon.Hammerflow.loadFirstValidTomlFile({
+--   "flow.toml",
+-- })
 
-hs.alert('loaded')
+-- if spoon.Hammerflow.auto_reload then
+--   hs.loadSpoon("ReloadConfiguration")
+--   -- set any paths for auto reload
+--   -- spoon.ReloadConfiguration.watch_paths = { '../.hammerspoon' }
+--   spoon.ReloadConfiguration:start()
+-- end
