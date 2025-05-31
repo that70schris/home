710c788021ed32a7b3ae79eb2db7e60a6213a9df
that70schris <chris@bailey.mx>
2025-05-31 20:55:02 +0000

    updates

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,7 +3,14 @@ hs.allowAppleScript(true)
 hs.loadSpoon('clear')
 hs.loadSpoon('enter')
 hs.loadSpoon('escape')
+hs.pathwatcher.new('../.hammerspoon/init.lua', hs.reload):start()
 hs.loadSpoon('Hammerflow')
-spoon.Hammerflow.loadFirstValidTomlFile({
-  'flow.toml',
-})
+-- spoon.Hammerflow.loadFirstValidTomlFile({
+--   'flow.toml',
+-- })
+
+hs.notify.show(
+  'Hammerspoon',
+  'Reloading configuration',
+  ''
+)
