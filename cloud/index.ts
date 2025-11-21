9e80204c55f1a5380bca739768e07c8d5748b8dd
that70schris <chris@bailey.mx>
2025-11-21 22:29:17 +0000

    updates

--- cloud/index.ts
+++ cloud/index.ts
@@ -1,10 +1,10 @@
 import { _Cluster } from './kubernetes';
-import { Homebridge } from './kubernetes/kubes';
+import { Homebridge, Plex } from './kubernetes/kubes';
 
 new _Cluster('berry', {
   domain: 'bailey.mx',
   includes: [
     new Homebridge(),
-    // new Plex(),
+    new Plex(),
   ],
 });
