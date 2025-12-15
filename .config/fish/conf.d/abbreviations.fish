56c9afed05c96114f68af72741677bbf8d43bf1f
that70schris <chris@bailey.mx>
2025-12-15 04:25:14 +0000

    updates

---
+++ .config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,34 @@
+abbr abort git rebase --abort
+abbr add git add
+abbr amend git commit --amend
+abbr ammend git commit -a --amend --no-edit
+abbr bottle brew bundle dump --force
+abbr c gcloud
+abbr check git checkout
+abbr clean git clean -fdx
+abbr cloud gcloud
+abbr commit git commit -am
+abbr d docker
+abbr dc docker compose
+abbr diff git difftool
+abbr fetch git fetch --prune
+abbr fire firebase
+abbr g git
+abbr gs git status --short
+abbr k kubecolor
+abbr kube kubecolor
+abbr kubectl kubecolor
+abbr list git branch
+abbr ls ls -a
+abbr merge git merge
+abbr pick git cherry-pick
+abbr pull git pull
+abbr push git push
+abbr rebase git rebase
+abbr reset git reset
+abbr roots 'git log --pretty=format:"%h %an %ar: %s" --graph'
+abbr show git showtool
+abbr skip git rebase --skip
+abbr stash git stash
+abbr tool git mergetool
+abbr trim git branch -D
