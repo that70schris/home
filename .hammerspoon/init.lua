4149a159d1b4d07f7e1e3c702eb3538e34dc0809
Chris Bailey <chris@bailey.mx>
2026-04-19 23:13:47 +0000

    updates

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,3 +4,32 @@ hs.loadSpoon('clear')
 hs.loadSpoon('enter')
 hs.loadSpoon('escape')
 hs.loadSpoon('finder')
+
+hs.eventtap.new({
+    hs.eventtap.event.types.flagsChanged,
+    hs.eventtap.event.types.keyDown,
+    hs.eventtap.event.types.keyUp,
+  },
+  function(e)
+    print(
+      hs.eventtap.event.types[e:getType()],
+      hs.keycodes.map[e:getKeyCode()],
+      e:getFlags().cmd,
+      e:getFlags().alt,
+      e:getFlags().shift,
+      hs.inspect(e:systemKey()),
+      '')
+  end
+):start()
+
+-- flagsChanged	cmd	true	true
+-- flagsChanged	cmd	nil	nil
+-- keyDown	escape	nil	nil
+-- keyUp	escape	nil	nil
+
+-- flagsChanged	cmd	true	nil
+-- flagsChanged	cmd	true	true
+-- flagsChanged	cmd	nil	nil
+-- keyDown	escape	nil	nil
+-- keyUp	escape	nil	nil
+-- flagsChanged	cmd	nil	nil
