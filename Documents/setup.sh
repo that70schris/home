25c53d1239e271454ae9dac81f1d6dec97650903
Chris Bailey <chris@bailey.mx>
2026-05-14 17:20:59 +0000

    publish

---
+++ Documents/setup.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+export NONINTERACTIVE=1
+# xcode-select --install
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+
+make brew
+brew install git
+brew install gh
+
+git init
+git remote add origin git@github.com:that70schris/-
+gh auth status || gh auth login \
+	--git-protocol ssh \
+	--hostname github.com \
+	--clipboard \
+	--web \
+
+git fetch
+git checkout main --force
+git submodule update --init --recursive
+
+brew bundle install --global
+kiro-cli integrations install input-method
+make starship
