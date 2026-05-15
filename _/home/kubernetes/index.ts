2b790cfaeaf158adc6f3f6ded10d04d020b9e16f
Chris Bailey <chris@bailey.mx>
2026-05-15 22:48:08 +0000

    publish

--- _/home/kubernetes/index.ts
+++ _/home/kubernetes/index.ts
@@ -1,13 +1,12 @@
 import { _Cluster } from '../../shared/models/kubernetes'
 import { Homebridge, Plex } from '../../shared/models/kubernetes/kubes'
-import { Homey } from '../../shared/models/kubernetes/kubes/Homey'
 
 new _Cluster('berry', {
   domain: 'bailey.mx',
   ip: '195.168.0.5',
   kubes: [
     new Homebridge(),
-    new Homey(),
+    // new Homey(),
     new Plex(),
   ],
 }).index
