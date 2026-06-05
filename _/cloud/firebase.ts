3521be74684445d1250ace45960eb5704407a952
Chris Bailey <chris@bailey.mx>
2026-06-05 13:38:07 +0000

    publish

--- _/cloud/firebase.ts
+++ _/cloud/firebase.ts
@@ -46,7 +46,7 @@ export class Firebase {
 
   static text = new _Record(
     'firebase.hostwriter.app', {
-      hostname: 'hostwriter.app',
+      domain: 'hostwriter.app',
       type: 'TXT',
       content: 'hosting-site=hostwriter',
     },
